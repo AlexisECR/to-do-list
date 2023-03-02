ed4199f37bb4fa83aa3a31f3b652e8d1b9f8ac61
AlexisECR <alexis.eduardo07@gmail.com>
2023-03-01 23:30:49 -0800

    new practices

--- Practica2-todo-tsc/assets/typescript/index.ts
+++ Practica2-todo-tsc/assets/typescript/index.ts
@@ -1,42 +1,37 @@
-const btnAdd: HTMLElement | null = document.getElementById('btnAdd');
+const btnAdd: HTMLButtonElement = document.getElementById('btnAdd') as HTMLButtonElement;
 const newTask: HTMLInputElement = document.getElementById("newTask") as HTMLInputElement;
-const containerList : HTMLElement | null = document.getElementById("containerList");
+const containerList: HTMLDivElement = document.getElementById("containerList") as HTMLDivElement;
 
-if(btnAdd){
-    btnAdd.addEventListener('click',function (e: MouseEvent){
-        let description:string = newTask.value;
+    btnAdd.addEventListener('click', function (e: MouseEvent) {
+        let description: string = newTask.value;
         createTask(description);
         newTask.value = "";
     })
-}
-else{console.log("Error: No se encontraron algunos elementos...")}
 
+function createTask(description: string): void {
 
-function createTask(description:string){
-    if(!description.trim()){
+    if (!description.trim()) {
         alert('Debes agregar un texto valido');
-        return ;
+        return;
     }
 
-    const task:HTMLElement = document.createElement('div');
+    const task: HTMLDivElement = document.createElement('div');
     task.setAttribute('class', 'task');
 
-    const taskDescription:HTMLElement = document.createElement('span');
+    const taskDescription: HTMLSpanElement = document.createElement('span');
     taskDescription.setAttribute('class', 'task-description');
     taskDescription.textContent = description;
 
-    const taskDeleteButton: HTMLElement = document.createElement('button');
+    const taskDeleteButton: HTMLButtonElement = document.createElement('button');
     taskDeleteButton.setAttribute('class', 'task-delete-button');
     taskDeleteButton.textContent = 'Eliminar';
-    taskDeleteButton.onclick = function(){
-        //document.getElementById("containerList")?.removeChild(task);
-    if(containerList){
+    taskDeleteButton.onclick = function () {    
         containerList.removeChild(task);
     }
-    }
-   
+
     task.appendChild(taskDescription)
     task.appendChild(taskDeleteButton);
 
     document.getElementById("containerList")?.appendChild(task);
+    
 }
