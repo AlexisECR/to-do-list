8048edb3567e4f8f0d66673cb7253bfb875c60a1
AlexisECR <alexis.eduardo07@gmail.com>
2023-03-09 08:56:14 -0800

    modify practices

--- Practica5-OrderList-New/assets/ts/main.js
+++ Practica5-OrderList-New/assets/ts/main.js
@@ -48,18 +48,198 @@ var productosDummy = {
             "images": [
                 "https://i.dummyjson.com/data/products/3/1.jpg"
             ]
+        },
+        {
+            "id": 4,
+            "title": "OPPOF19",
+            "description": "OPPO F19 is officially announced on April 2021.",
+            "price": 280,
+            "discountPercentage": 17.91,
+            "rating": 4.3,
+            "stock": 0,
+            "brand": "OPPO",
+            "category": "smartphones",
+            "images": [
+                "https://i.dummyjson.com/data/products/4/1.jpg",
+                "https://i.dummyjson.com/data/products/4/2.jpg",
+                "https://i.dummyjson.com/data/products/4/3.jpg",
+                "https://i.dummyjson.com/data/products/4/4.jpg",
+                "https://i.dummyjson.com/data/products/4/thumbnail.jpg"
+            ]
+        },
+        {
+            "id": 5,
+            "title": "Huawei P30",
+            "description": "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+            "price": 499,
+            "discountPercentage": 10.58,
+            "rating": 4.09,
+            "stock": 32,
+            "brand": "Huawei",
+            "category": "smartphones",
+            "images": [
+                "https://i.dummyjson.com/data/products/5/1.jpg",
+                "https://i.dummyjson.com/data/products/5/2.jpg",
+                "https://i.dummyjson.com/data/products/5/3.jpg"
+            ]
+        },
+        {
+            "id": 6,
+            "title": "MacBook Pro",
+            "description": "MacBook Pro 2021 with mini-LED display may launch between September, November",
+            "price": 1749,
+            "discountPercentage": 11.02,
+            "rating": 4.57,
+            "stock": 0,
+            "brand": "Apple",
+            "category": "laptops",
+            "images": [
+                "https://i.dummyjson.com/data/products/6/1.png",
+                "https://i.dummyjson.com/data/products/6/2.jpg",
+                "https://i.dummyjson.com/data/products/6/3.png",
+                "https://i.dummyjson.com/data/products/6/4.jpg"
+            ]
+        },
+        {
+            "id": 7,
+            "title": "Samsung Galaxy Book",
+            "description": "Samsung Galaxy Book S (2020) Laptop With Intel Lakefield Chip, 8GB of RAM Launched",
+            "price": 1499,
+            "discountPercentage": 4.15,
+            "rating": 4.25,
+            "stock": 50,
+            "brand": "Samsung",
+            "category": "laptops",
+            "images": [
+                "https://i.dummyjson.com/data/products/7/1.jpg",
+                "https://i.dummyjson.com/data/products/7/2.jpg",
+                "https://i.dummyjson.com/data/products/7/3.jpg",
+                "https://i.dummyjson.com/data/products/7/thumbnail.jpg"
+            ]
+        },
+        {
+            "id": 8,
+            "title": "Microsoft Surface Laptop 4",
+            "description": "Style and speed. Stand out on HD video calls backed by Studio Mics. Capture ideas on the vibrant touchscreen.",
+            "price": 1499,
+            "discountPercentage": 10.23,
+            "rating": 4.43,
+            "stock": 68,
+            "brand": "Microsoft Surface",
+            "category": "laptops",
+            "images": [
+                "https://i.dummyjson.com/data/products/8/1.jpg",
+                "https://i.dummyjson.com/data/products/8/2.jpg",
+                "https://i.dummyjson.com/data/products/8/3.jpg",
+                "https://i.dummyjson.com/data/products/8/4.jpg",
+                "https://i.dummyjson.com/data/products/8/thumbnail.jpg"
+            ]
+        },
+        {
+            "id": 9,
+            "title": "Infinix INBOOK",
+            "description": "Infinix Inbook X1 Ci3 10th 8GB 256GB 14 Win10 Grey – 1 Year Warranty",
+            "price": 1099,
+            "discountPercentage": 11.83,
+            "rating": 4.54,
+            "stock": 96,
+            "brand": "Infinix",
+            "category": "laptops",
+            "images": [
+                "https://i.dummyjson.com/data/products/9/1.jpg",
+                "https://i.dummyjson.com/data/products/9/2.png",
+                "https://i.dummyjson.com/data/products/9/3.png",
+                "https://i.dummyjson.com/data/products/9/4.jpg",
+                "https://i.dummyjson.com/data/products/9/thumbnail.jpg"
+            ]
+        },
+        {
+            "id": 10,
+            "title": "HP Pavilion 15-DK1056WM",
+            "description": "HP Pavilion 15-DK1056WM Gaming Laptop 10th Gen Core i5, 8GB, 256GB SSD, GTX 1650 4GB, Windows 10",
+            "price": 1099,
+            "discountPercentage": 6.18,
+            "rating": 4.43,
+            "stock": 0,
+            "brand": "HP Pavilion",
+            "category": "laptops",
+            "images": [
+                "https://i.dummyjson.com/data/products/10/1.jpg",
+                "https://i.dummyjson.com/data/products/10/2.jpg",
+                "https://i.dummyjson.com/data/products/10/3.jpg",
+                "https://i.dummyjson.com/data/products/10/thumbnail.jpeg"
+            ]
         }
     ]
 };
 var productosConvertidos = productosDummy.products.map(function (producto) {
     return {
-        titulo: producto.title,
-        precio: producto.price,
-        imagenes: producto.images
+        title: producto.title,
+        price: producto.price,
+        stock: producto.stock,
+        category: producto.category,
+        images: producto.images
     };
 });
 productosConvertidos.forEach(function (producto) {
-    console.log(producto.titulo);
-    console.log(producto.precio);
-    console.log(producto.imagenes[1]);
+    var article = document.createElement('div');
+    article.setAttribute('class', 'article');
+    articleImage(producto.images[0], article);
+    articleDescription(producto.title, producto.category, article);
+    articlePrice(producto.price, producto.stock, article);
+    addArticle(article);
 });
+function articleImage(img, article) {
+    var articleImage = document.createElement('div');
+    articleImage.setAttribute('class', 'article-image');
+    var image = document.createElement('img');
+    image.setAttribute('src', img);
+    articleImage.appendChild(image);
+    article.appendChild(articleImage);
+}
+function articleDescription(title, category, article) {
+    var articleDescription = document.createElement('div');
+    articleDescription.setAttribute('class', 'article-description');
+    var articleName = document.createElement('span');
+    articleName.setAttribute('class', 'article-name');
+    articleName.innerText = title;
+    var icono = document.createElement('i');
+    icono.setAttribute('class', 'bi bi-tag');
+    var articleCategory = document.createElement('span');
+    articleCategory.setAttribute('class', 'article-category');
+    articleCategory.innerText = category;
+    articleDescription.appendChild(articleName);
+    articleDescription.appendChild(icono);
+    articleDescription.appendChild(articleCategory);
+    article.appendChild(articleDescription);
+}
+function articlePrice(price, existenceStock, article) {
+    var articlePrice = document.createElement('div');
+    articlePrice.setAttribute('class', 'article-price');
+    var priceBadge = document.createElement('span');
+    priceBadge.setAttribute('class', 'price-badge');
+    priceBadge.innerText = "$" + price;
+    var stock = document.createElement('span');
+    typeStock(existenceStock, stock);
+    articlePrice.appendChild(priceBadge);
+    articlePrice.appendChild(stock);
+    article.appendChild(articlePrice);
+}
+function addArticle(article) {
+    var _a;
+    (_a = document.getElementById("listArticles")) === null || _a === void 0 ? void 0 : _a.appendChild(article);
+}
+function typeStock(amountStock, stock) {
+    if (amountStock >= 50) {
+        stock.setAttribute('class', 'instock');
+        stock.innerText = "INSTOCK";
+    }
+    else if (amountStock < 50 && amountStock > 0) {
+        stock.setAttribute('class', 'lowstock');
+        stock.innerText = "LOWSTOCK";
+    }
+    else {
+        stock.setAttribute('class', 'outofstock');
+        stock.innerText = "OUTOFSTOCK";
+    }
+}
--- Practica5-OrderList-New/assets/ts/main.ts
+++ Practica5-OrderList-New/assets/ts/main.ts
@@ -48,26 +48,232 @@ let productosDummy = {
             "images": [
                 "https://i.dummyjson.com/data/products/3/1.jpg"
             ]
+        },
+        {
+            "id": 4,
+            "title": "OPPOF19",
+            "description": "OPPO F19 is officially announced on April 2021.",
+            "price": 280,
+            "discountPercentage": 17.91,
+            "rating": 4.3,
+            "stock": 0,
+            "brand": "OPPO",
+            "category": "smartphones",
+            "images": [
+                "https://i.dummyjson.com/data/products/4/1.jpg",
+                "https://i.dummyjson.com/data/products/4/2.jpg",
+                "https://i.dummyjson.com/data/products/4/3.jpg",
+                "https://i.dummyjson.com/data/products/4/4.jpg",
+                "https://i.dummyjson.com/data/products/4/thumbnail.jpg"
+            ]
+        },
+        {
+            "id": 5,
+            "title": "Huawei P30",
+            "description": "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+            "price": 499,
+            "discountPercentage": 10.58,
+            "rating": 4.09,
+            "stock": 32,
+            "brand": "Huawei",
+            "category": "smartphones",
+            "images": [
+                "https://i.dummyjson.com/data/products/5/1.jpg",
+                "https://i.dummyjson.com/data/products/5/2.jpg",
+                "https://i.dummyjson.com/data/products/5/3.jpg"
+            ]
+        },
+        {
+            "id": 6,
+            "title": "MacBook Pro",
+            "description": "MacBook Pro 2021 with mini-LED display may launch between September, November",
+            "price": 1749,
+            "discountPercentage": 11.02,
+            "rating": 4.57,
+            "stock": 0,
+            "brand": "Apple",
+            "category": "laptops",
+            "images": [
+                "https://i.dummyjson.com/data/products/6/1.png",
+                "https://i.dummyjson.com/data/products/6/2.jpg",
+                "https://i.dummyjson.com/data/products/6/3.png",
+                "https://i.dummyjson.com/data/products/6/4.jpg"
+            ]
+        },
+        {
+            "id": 7,
+            "title": "Samsung Galaxy Book",
+            "description": "Samsung Galaxy Book S (2020) Laptop With Intel Lakefield Chip, 8GB of RAM Launched",
+            "price": 1499,
+            "discountPercentage": 4.15,
+            "rating": 4.25,
+            "stock": 50,
+            "brand": "Samsung",
+            "category": "laptops",
+            "images": [
+                "https://i.dummyjson.com/data/products/7/1.jpg",
+                "https://i.dummyjson.com/data/products/7/2.jpg",
+                "https://i.dummyjson.com/data/products/7/3.jpg",
+                "https://i.dummyjson.com/data/products/7/thumbnail.jpg"
+            ]
+        },
+        {
+            "id": 8,
+            "title": "Microsoft Surface Laptop 4",
+            "description": "Style and speed. Stand out on HD video calls backed by Studio Mics. Capture ideas on the vibrant touchscreen.",
+            "price": 1499,
+            "discountPercentage": 10.23,
+            "rating": 4.43,
+            "stock": 68,
+            "brand": "Microsoft Surface",
+            "category": "laptops",
+            "images": [
+                "https://i.dummyjson.com/data/products/8/1.jpg",
+                "https://i.dummyjson.com/data/products/8/2.jpg",
+                "https://i.dummyjson.com/data/products/8/3.jpg",
+                "https://i.dummyjson.com/data/products/8/4.jpg",
+                "https://i.dummyjson.com/data/products/8/thumbnail.jpg"
+            ]
+        },
+        {
+            "id": 9,
+            "title": "Infinix INBOOK",
+            "description": "Infinix Inbook X1 Ci3 10th 8GB 256GB 14 Win10 Grey – 1 Year Warranty",
+            "price": 1099,
+            "discountPercentage": 11.83,
+            "rating": 4.54,
+            "stock": 96,
+            "brand": "Infinix",
+            "category": "laptops",
+            "images": [
+                "https://i.dummyjson.com/data/products/9/1.jpg",
+                "https://i.dummyjson.com/data/products/9/2.png",
+                "https://i.dummyjson.com/data/products/9/3.png",
+                "https://i.dummyjson.com/data/products/9/4.jpg",
+                "https://i.dummyjson.com/data/products/9/thumbnail.jpg"
+            ]
+        },
+        {
+            "id": 10,
+            "title": "HP Pavilion 15-DK1056WM",
+            "description": "HP Pavilion 15-DK1056WM Gaming Laptop 10th Gen Core i5, 8GB, 256GB SSD, GTX 1650 4GB, Windows 10",
+            "price": 1099,
+            "discountPercentage": 6.18,
+            "rating": 4.43,
+            "stock": 0,
+            "brand": "HP Pavilion",
+            "category": "laptops",
+            "images": [
+                "https://i.dummyjson.com/data/products/10/1.jpg",
+                "https://i.dummyjson.com/data/products/10/2.jpg",
+                "https://i.dummyjson.com/data/products/10/3.jpg",
+                "https://i.dummyjson.com/data/products/10/thumbnail.jpeg"
+            ]
         }
     ]
 };
 
+type ProductoDummy = {
+    title: string;
+    price:number;
+    stock: number;
+    category: string;
+    images: string[];
+};  
+
 const productosConvertidos = productosDummy.products.map((producto) => {
     return {
-        titulo: producto.title,
-        precio: producto.price,
-        imagenes: producto.images
+        title: producto.title,
+        price: producto.price,
+        stock: producto.stock,
+        category: producto.category,
+        images: producto.images
     }
 })
 
+productosConvertidos.forEach((producto: ProductoDummy) => {
 
-productosConvertidos.forEach((producto: any) => {
-    console.log(producto.titulo);
-    console.log(producto.precio);
-    console.log(producto.imagenes[1])
+    let article: HTMLDivElement = document.createElement('div');
+    article.setAttribute('class','article');
+    articleImage(producto.images[0], article);
+    articleDescription(producto.title,producto.category, article);
+    articlePrice(producto.price, producto.stock, article);
+    addArticle(article);
+
 });
 
-type ProductoDummy = {
-    title:string;
-    price:number;
-};  
+function articleImage(img:string, article:HTMLDivElement): void{
+
+    let articleImage: HTMLDivElement = document.createElement('div');
+    articleImage.setAttribute('class','article-image');
+
+    let image: HTMLDivElement = document.createElement('img');
+    image.setAttribute('src', img);
+
+    articleImage.appendChild(image);
+    article.appendChild(articleImage);
+
+}
+
+function articleDescription (title:string, category:string, article:HTMLDivElement):void {
+
+    let articleDescription: HTMLDivElement = document.createElement('div');
+    articleDescription.setAttribute('class','article-description');
+
+    let articleName: HTMLSpanElement = document.createElement('span');
+    articleName.setAttribute('class','article-name');
+    articleName.innerText = title;
+
+    let icono: HTMLElement = document.createElement('i');
+    icono.setAttribute('class','bi bi-tag');
+
+    let articleCategory: HTMLSpanElement = document.createElement('span');
+    articleCategory.setAttribute('class','article-category');
+    articleCategory.innerText = category;
+
+    articleDescription.appendChild(articleName);
+    articleDescription.appendChild(icono);
+    articleDescription.appendChild(articleCategory);
+    article.appendChild(articleDescription);
+
+}
+
+function articlePrice (price:number,existenceStock:number, article:HTMLDivElement):void {
+   
+    let articlePrice: HTMLDivElement = document.createElement('div');
+    articlePrice.setAttribute('class','article-price');
+
+    let priceBadge: HTMLSpanElement = document.createElement('span');
+    priceBadge.setAttribute('class','price-badge');
+    priceBadge.innerText = "$"+price;
+
+    let stock: HTMLSpanElement = document.createElement('span');
+    typeStock(existenceStock,stock);
+
+    articlePrice.appendChild(priceBadge);
+    articlePrice.appendChild(stock);
+    article.appendChild(articlePrice);
+
+}
+
+function addArticle (article: HTMLDivElement) {
+    document.getElementById("listArticles")?.appendChild(article);
+}
+
+function typeStock(amountStock:number,stock:HTMLSpanElement){
+    if(amountStock >= 50)
+    {
+        stock.setAttribute('class','instock');
+        stock.innerText = "INSTOCK";
+    }
+    else if(amountStock < 50 && amountStock > 0)
+    {
+        stock.setAttribute('class','lowstock');
+        stock.innerText = "LOWSTOCK";
+    }
+    else
+    {
+        stock.setAttribute('class','outofstock');
+        stock.innerText = "OUTOFSTOCK"; 
+    }
+}
